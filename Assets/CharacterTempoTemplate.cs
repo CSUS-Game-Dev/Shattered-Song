a777c33d40558927a00a2d19ba8f4b698fc4bb0b
Bryce Collins <brycecollins0@gmail.com>
2018-02-24 14:14:10 -0800

    Starting to work on the initiative thing

---
+++ Assets/CharacterTempoTemplate.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterTempoTemplate {
+
+	private int tempo;
+	private int nextTurnModifier; 
+	private int timeWaiting;
+
+	public int Tempo
+    {
+        get
+        {
+            return tempo;
+        }
+    }
+
+	public int NextTurnModifier
+	{
+		get{
+			return nextTurnModifier;
+		}
+		set{
+			nextTurnModifier = value;
+		}
+	}
+
+	public CharacterTempoTemplate(){
+
+	}
+
+	public void takeTurn(){
+		nextTurnModifier = 0;
+	}
+
+	public void takeTurn(int nextModifier){
+		nextTurnModifier = nextModifier;
+	}
+
+	public int getNextTurnWait(){
+		return tempo + nextTurnModifier;
+	}
+
+
+}
---
+++ Assets/TurnManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnManager : MonoBehaviour {
+
+	private List<CharacterTempoTemplates> characters;
+
+	// Use this for initialization
+	void Start () {
+		characters = new List<CharacterTempoTemplates>();
+		characters
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("space")){
+			characters[0].takeTurn();
+			updateList();
+		}
+	}
+
+	public void updateList(){
+		bool noCharacterIsReady = true;
+
+		while(noCharacterIsReady){
+		for(int i = 0; i < character.Count; i++){
+			if()
+		}
+	}
+
+
+}
