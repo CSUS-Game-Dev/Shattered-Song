48cad0403fa9cbc9408dcd870723ec300f0c30fc
Bryce Collins <brycecollins0@gmail.com>
2018-02-25 10:03:16 -0800

    Continued building on the tempo project

--- Assets/CharacterTempoTemplate.cs
+++ Assets/CharacterTempoTemplate.cs
@@ -4,46 +4,27 @@ using UnityEngine;
 
 public class CharacterTempoTemplate {
 
-	public int tempo
-    {
-        get
-        {
-            return tempo;
-        }
-    }
+	public static int DEFAULT_TEMPO = 9;
+	public static string DEFAULT_NAME = "Generic_Name";
 
-	public int nextTurnModifier
-	{
-		get{
-			return nextTurnModifier;
-		}
-		set{
-			nextTurnModifier = value;
-		}
-	}
+	public int tempo;
+	public int nextTurnModifier;
+	public string charName;
+	private int timeWaiting;
 
-	public string charName
-	{
-		get{
-			return charName;
-		}
-		set{
-			charName = value;
-		}
-	}
-
-	public int timeWaiting
-	{
-		get{
-			return timeWaiting;
-		}
-		set{
-			timeWaiting = value;
-		}
-	}
 
 	public CharacterTempoTemplate(){
+		tempo = DEFAULT_TEMPO;
+		charName = DEFAULT_NAME;
+		nextTurnModifier = 0;
+		timeWaiting = 0;
+	}
 
+	public CharacterTempoTemplate(int startTempo, string name){
+		tempo = startTempo;
+		charName = name;
+		nextTurnModifier = 0;
+		timeWaiting = 0;
 	}
 
 	public void takeTurn(){
@@ -53,6 +34,7 @@ public class CharacterTempoTemplate {
 
 	public void takeTurn(int nextModifier){
 		nextTurnModifier = nextModifier;
+		timeWaiting = 0;
 	}
 
 	public int getNextTurnWait(){
@@ -61,6 +43,12 @@ public class CharacterTempoTemplate {
 
 	public void incTimeWaiting(){
 			timeWaiting++;
+	}
+
+	public int timeToNextTurn(){
+		int result = tempo + nextTurnModifier - timeWaiting;
+		if(result < 0){ result = 0;}
+		return result;
 	}
 
 	//Quick test to see if the player is ready to go
--- Assets/TurnManager.cs
+++ Assets/TurnManager.cs
@@ -6,6 +6,7 @@ public class TurnManager : MonoBehaviour {
 
 	private List<CharacterTempoTemplate> characters;
 	private List<PlayerTurn> turnOrder;
+	private static int TURN_TRACKER_COUNT = 10;
 
 	// Use this for initialization
 	void Start () {
@@ -16,8 +17,20 @@ public class TurnManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKeyDown("space")){
-			characters[0].takeTurn();
+			takeTurn();
+		}
+		if(Input.GetKeyDown("w")){
+			takeTurn(10);
+		}
+		if(Input.GetKeyDown("s")){
+			characters.Add(new CharacterTempoTemplate());
+			characters.Add(new CharacterTempoTemplate(4, "Raven"));
+			characters.Add(new CharacterTempoTemplate(5, "Ignis"));
 			updateList();
+			Debug.Log("TurnOrder list length: " + turnOrder.Count);
+		}
+		if(Input.GetKeyDown("p")){
+			printTurnOrder();
 		}
 	}
 
@@ -25,18 +38,93 @@ public class TurnManager : MonoBehaviour {
 		sortByTurnOrder();
 	}
 
+	public void takeTurn(){
+		//while the character first in the turn order is not ready to go, update time
+		if(characters.Count > 0 || turnOrder.Count > 0){
+			while(!turnOrder[0].character.readyToGo()){
+				for(int i = 0; i < characters.Count; i++){
+					characters[i].incTimeWaiting();
+				}
+			}
+
+			turnOrder[0].character.takeTurn();
+			updateList();
+		}
+		else{
+			Debug.Break();
+			Debug.Log("Either the characters or the turnorder list is empty.");
+		}
+	}
+
+		public void takeTurn(int nextTurnMod ){
+		if(characters.Count > 0 || turnOrder.Count > 0){
+			while(!turnOrder[0].character.readyToGo()){
+				for(int i = 0; i < characters.Count; i++){
+					characters[i].incTimeWaiting();
+				}
+			}
+
+			turnOrder[0].character.takeTurn(nextTurnMod);
+			updateList();
+		}
+		else{
+			Debug.Break();
+			Debug.Log("Either the characters or the turnorder list is empty.");
+		}
+	}
 
 	private void sortByTurnOrder(){
 		List<PlayerTurn> newTurnOrder = new List<PlayerTurn>();
 
+		while(newTurnOrder.Count < TURN_TRACKER_COUNT){
+			newTurnOrder.Add(findLowestTurnCostNotInList(newTurnOrder));
+		}
 
+		turnOrder = newTurnOrder;
 	}
 	
+	public PlayerTurn findLowestTurnCostNotInList(List<PlayerTurn> newTurnOrder){
+		//Temp turn while we iterate through the characters list
+		PlayerTurn newTurn;
+		PlayerTurn resultTurn = new PlayerTurn();
+		resultTurn.turnInList = int.MaxValue;
+
+		//Finds the least cost turn for each character in the characters list
+		//Returns the one that costs the least that is NOT already in the turnOrder list
+		//Right now, in the case of a tie, the character that comes first in the 'characters' list goes first
+		for(int i = 0; i < characters.Count; i++){
+
+			newTurn.character = characters[i];
+			newTurn.turnInList = 0;
+
+			while(playerTurnInList(newTurn, newTurnOrder)){
+				newTurn.turnInList++;
+			}
+
+			if(resultTurn.character == null || newTurn.timeUntilTurn() < resultTurn.timeUntilTurn()){
+				resultTurn = newTurn;
+			}
+		}
+
+		return resultTurn;
+	}
+
+	//returns true if the PlayerTurn is in the list
+	private bool playerTurnInList(PlayerTurn playerTurn, List<PlayerTurn> turnList){
+
+		for(int i = 0; i < turnList.Count; i++){
+			if(turnList[i].isEqual(playerTurn)){
+				return true;
+			}
+		}
+		return false;
+	}
+
 	//Largely for development purposes
 	//Helps us test whether or not the algorithm is working as intended
 	public void printTurnOrder(){
 		for(int i = 0; i < turnOrder.Count; i++){
-			Debug.Log("Character " + characters[i].charName + "goes in turn position" + (i+1));
+			Debug.Log(turnOrder[i].character.charName + " goes at turn: " + (i+1));
 		}
 	}
 
@@ -46,6 +134,8 @@ public class TurnManager : MonoBehaviour {
 		//This is the turn number in the list i.e. the first, second, or third time in the list that the character moves.
 		// The "time" the turn happens is equal to character.nextTurnModifier + (turnInList * character.tempo)
 		public int turnInList;
+
+
 
 		public PlayerTurn(CharacterTempoTemplate c, int turn){
 			turnInList = turn;
@@ -59,6 +149,10 @@ public class TurnManager : MonoBehaviour {
 			else{
 				return false;
 			}
+		}
+
+		public int timeUntilTurn(){
+			return character.timeToNextTurn() + character.tempo * turnInList;
 		}
 	}
 
