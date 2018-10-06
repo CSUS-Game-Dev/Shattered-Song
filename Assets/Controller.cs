bea1e412780bd2618727707cef6f98e5644bc5d4
Bryce Collins <brycecollins0@gmail.com>
2018-10-06 10:41:53 -0700

    Cleaned up the file system. Deleted old items

---
+++ Assets/Controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+
+    private Cursor activeCursor;
+
+    private MenuCursor menuCursor;
+    private MapCursor mapCursor;
+    private DialogueCursor dialogueCursor;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
